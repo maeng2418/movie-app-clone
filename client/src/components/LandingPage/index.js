a2fecb930a0b5a3310699232cfa78ddd24b25d3e
maeng2418 <maeng2418@github.com>
2020-03-26 16:04:25 +0900

    4. Load More Button

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -9,17 +9,30 @@ function LandingPage(props) {
 
   const [Movies, setMovies] = useState([]);
   const [MainMoiveImage, setMainMoiveImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
 
+    fetchMovies(endpoint);
+
+  }, []);
+
+  const fetchMovies = (endpoint) => {
     fetch(endpoint)
       .then(response => response.json())
       .then(response => {
         setMovies([...Movies, ...response.results]);
-        setMainMoiveImage(response.results[0])
-      })
-  }, []);
+        if (!MainMoiveImage) setMainMoiveImage(response.results[0]);
+        setCurrentPage(response.page);
+      }
+      )
+  };
+
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
+    fetchMovies(endpoint)
+  }
 
   return (
     <div className={styles.mainContainer}>
@@ -45,7 +58,7 @@ function LandingPage(props) {
         <Row gutter={[16, 16]}> {/* 카드 위 아래 여백 */}
           {Movies && Movies.map((movie, index) => (
             <React.Fragment key={index}>
-              <GridCards 
+              <GridCards
                 image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
                 movieId={movie.id}
                 moiveName={movie.orginal_title}
@@ -58,7 +71,7 @@ function LandingPage(props) {
       </div>
 
       <div className={styles.subContainer}>
-        <button>Load More</button>
+        <button onClick={loadMoreItems}>Load More</button>
       </div>
 
     </div>
