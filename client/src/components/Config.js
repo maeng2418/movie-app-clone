20604ead71662d7d0c8cd1403304eb9837e09700
maeng2418 <maeng2418@github.com>
2020-03-26 15:32:44 +0900

    1. Setting MovieDB API URL

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,1 +1,7 @@
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '5d2bb01cfb145f1f21c880794e27d6d7';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'; 
