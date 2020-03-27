cccaca5e01f10156c67508a5b63fa3a91e3d8691
maeng2418 <maeng2418@github.com>
2020-03-27 17:32:31 +0900

    Delete server index router

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser'); // Body 데이터를 분석(parse)해서 req.body로 출력해주는 것
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var faovriteRouter = require('./routes/favorite');
 
@@ -18,7 +17,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 // aplication/json
 app.use(bodyParser.json());
 
-app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/favorite', faovriteRouter);
 
